bbb180b3a78d9402eb3a85bfa09c1e2c0d3f5b1e
Naresh Kumar <naresh.kumar2@crowncommercial.gov.uk>
2021-12-09 10:20:11 +0000

    remove check for environment

--- logindirector/Program.cs
+++ logindirector/Program.cs
@@ -26,8 +26,7 @@ namespace logindirector
                 {
                     string envName = hostingContext.HostingEnvironment.EnvironmentName.ToString().ToLower();
                     var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
-                    if(!isDevelopment)
-                    {
+                   
                     var configg = config.Build();
                     var cloudServiceConfig = configg.GetSection("vcap").Get<CloudFoundryServicesOptions>();
                     var cf_aws_access_key_id = cloudServiceConfig.Services["user-provided"].First(s => s.Name == "aws-ssm").Credentials["aws_access_key_id"].Value;
@@ -37,7 +36,6 @@ namespace logindirector
                     Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", cf_aws_access_key_id);
                     Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", cf_aws_secret_access_key);
                     Environment.SetEnvironmentVariable("AWS_REGION", cf_aws_region);
-                    }
 
                     config.AddSystemsManager($"/{envName}", TimeSpan.FromMinutes(5));
 
